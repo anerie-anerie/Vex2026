f69cfc2f44cb968071ba727cb1ed8b80b554a42d
anerie <141164361+anerie-anerie@users.noreply.github.com>
2026-02-04 11:18:31 -0500

    auto ideas

--- auto.py
+++ auto.py
@@ -1,1 +1,7 @@
-#auto code
+'''
+auto code
+Approximately 12.67 inches to the bottom lip.
+Upper Center Goal: Approximately 8.01 inches to the bottom lip.
+Lower Center Goal: Approximately 0.5 inches.'''
+
+#auto routes: use 2 pre-loaded and score into upper central goal or low central goal -> intake more
