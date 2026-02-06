f47d4aa8899375adb40cace1a0c3c96d8359bdeb
anerie <141164361+anerie-anerie@users.noreply.github.com>
2026-02-06 11:32:09 -0500

    small fixes

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from vex import *
 import urandom
 import math
 
-brain = Brain()
+# Brain should be defined by default
+brain=Brain()
 
 # Robot configuration code
 controller_1 = Controller(PRIMARY)
@@ -17,18 +18,31 @@ right_drive_smart = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
 
 # Drivetrain (REQUIRED for auto)
 drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
-
+# wait for rotation sensor to fully initialize
 wait(30, MSEC)
 
+
+# Make random actually random
 def initializeRandomSeed():
-    wait(10, MSEC)
+    wait(100, MSEC)
     random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
     urandom.seed(int(random))
-
+      
+# Set random seed 
 initializeRandomSeed()
 
-wait(20, MSEC)
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
 print("\033[2J")
+
 #endregion VEXcode Generated Robot Configuration
 
 
@@ -80,11 +94,11 @@ def controller_1button_released():
 ############################################
 
 def drivercontrol():
-
+#left joystick forward and backwards, right joystick is right and left
     while True:
         
         forward = controller_1.axis3.position()
-        turn = controller_1.axis4.position()
+        turn = controller_1.axis1.position()
 
         # Deadzone
         if abs(forward) < DEADZONE:
@@ -110,10 +124,10 @@ def auto():
     brain.screen.clear_screen()
     brain.screen.set_cursor(1,1)
     brain.screen.print("AUTO STARTING")
-    drivetrain.set_drive_velocity(70, PERCENT)
+    drivetrain.set_drive_velocity(60, PERCENT)
 
     # Drive forward 30 inches
-    drivetrain.drive_for(FORWARD, 30, INCHES)
+    drivetrain.drive_for(REVERSE, 30, INCHES)
 
     # Turn left 45°
     drivetrain.turn_for(LEFT, 45, DEGREES)
@@ -128,7 +142,9 @@ def auto():
     leftIN.spin(FORWARD)
 
     # Drive forward while outtaking
-    drivetrain.drive_for(FORWARD, 7, INCHES)
+    drivetrain.drive_for(REVERSE, 7, INCHES)
+
+    wait(2000, MSEC)
 
     # Stop motors
     Upper.stop()
