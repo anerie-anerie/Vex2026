60673d6cc2e5e369fbef729fc8043bba95f3dc7c
anerie <141164361+anerie-anerie@users.noreply.github.com>
2026-02-06 11:27:47 -0500

    Implement autonomous driving sequence in auto.py

--- auto.py
+++ auto.py
@@ -5,3 +5,33 @@ Upper Center Goal: Approximately 8.01 inches to the bottom lip.
 Lower Center Goal: Approximately 0.5 inches.'''
 
 #auto routes: use 2 pre-loaded and score into upper central goal or low central goal -> intake more
+
+#starts right from the goal (put robot in backwards position, moves forward then turns left
+def auto():
+    brain.screen.clear_screen()
+    brain.screen.set_cursor(1,1)
+    brain.screen.print("AUTO STARTING")
+    drivetrain.set_drive_velocity(70, PERCENT)
+
+    # Drive forward 30 inches
+    drivetrain.drive_for(REVERSE, 30, INCHES)
+
+    # Turn left 45°
+    drivetrain.turn_for(RIGHT, 45, DEGREES)
+
+    # Start outtake
+    Upper.set_velocity(70, PERCENT)
+    leftIN.set_velocity(70, PERCENT)
+    rightIN.set_velocity(70, PERCENT)
+
+    Upper.spin(FORWARD)
+    rightIN.spin(REVERSE)
+    leftIN.spin(FORWARD)
+
+    # Drive forward while outtaking
+    drivetrain.drive_for(FORWARD, 7, INCHES)
+
+    # Stop motors
+    Upper.stop()
+    rightIN.stop()
+    leftIN.stop()
